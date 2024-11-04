2e1028c3508ab90c4f509d89e08ae43f02255f20
kemptwmail <kemptw@mail.uc.edu>
2024-11-03 23:21:25 -0500

    commit final

---
+++ WebApplication1/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            return Ok(new { name = "Tyler Kemp" });
+        }
+    }
+}
